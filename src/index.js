1a526b959b513bb73e2292764280238dd3bec84f
Gabriel Gualteros <Roblox4@gmail.com>
2021-06-09 19:18:05 -0400

    fixed store error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-// import store from './redux/store'
 
-const store = {
-    token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyMjcwMzc4MSwiaWF0IjoxNjIyNjY3NzgxfQ.ht-NPBgJBRI3FAbscD8si4ffkzuo1xX5DwouawHXIcw"
-}
+import App from './App';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Provider>,
+    <React.Fragment>
+        <App />
+    </React.Fragment>,
     document.getElementById('root')
 );
-
